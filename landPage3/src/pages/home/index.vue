bf7dc0e32400216d42cd865a382baf1a157086da
2644345145@qq.com <065998lcr>
2025-07-24 21:15:14 +0800

    【前端】代码提交 延时 1.5s

--- landPage3/src/pages/home/index.vue
+++ landPage3/src/pages/home/index.vue
@@ -32,12 +32,14 @@ export default {
       url: window.location.href || ''
     }
     // this.urlA = 'https://wri375.com/?t=1&d=gcereS2' // getCookie('url') ||  'https://wri375.com/?t=1&d=OUtWwWQa#/register'
-    this.urlA = getCookie('url') || 'https://wri375.com/?t=1&d=OUtWwWQa#/register'
-    console.log('urlA', this.urlA)
     postVisitAndClickApi(params).then(res => {
       console.log('访问量', res)
     }).finally(() => {
-      window.open(this.urlA, '_self') // '_blank' 表示在新窗口或标签页中打开
+      setTimeout(() => {
+        this.urlA = getCookie('url') || 'https://wri375.com/?t=1&d=OUtWwWQa#/register'
+        console.log('urlA', this.urlA)
+        window.open(this.urlA, '_self') // '_blank' 表示在新窗口或标签页中打开
+      }, 1500)
     })
   },
   beforeDestroy () {
